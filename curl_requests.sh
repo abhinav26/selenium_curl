4c57496b1579f3c6d38c1e8af0eda430abd8f0d8
Abhinav <abhinav@browserstack.com>
2013-09-13 17:57:05 +0530

    Removed the ruby file

--- curl_requests.sh
+++ curl_requests.sh
@@ -1,7 +1,10 @@
+session_id=`curl -X POST http://127.0.0.1:4444/wd/hub/session -d '{"desiredCapabilities":{"browserName":"firefox","platform":"MAC"}}'|awk -F'"' '{print $6}'`
 
+curl -X POST http://127.0.0.1:4444/wd/hub/session/$session_id/url -d '{"url":"http://www.google.com"}'
+curl -X POST http://127.0.0.1:4444/wd/hub/session/$session_id/element -d '{"using":"id","value":"gbqfq"}'
+curl -X POST http://127.0.0.1:4444/wd/hub/session/$session_id/element/0/value -d {"value":["browserstack"]}
+curl -X POST http://127.0.0.1:4444/wd/hub/session/$session_id/element -d '{"using":"id","value":"gbqfb"}'
+curl -X POST http://127.0.0.1:4444/wd/hub/session/$session_id/element/1/click
+sleep 3
 
-curl -X POST http://127.0.0.1:4444/wd/hub/session/$1/url -d '{"url":"http://www.google.com"}'
-curl -X POST http://127.0.0.1:4444/wd/hub/session/$1/element -d '{"using":"id","value":"gbqfq"}'
-curl -X POST http://127.0.0.1:4444/wd/hub/session/$1/element/0/value -d {"value":["browserstack"]}
-curl -X POST http://127.0.0.1:4444/wd/hub/session/$1/element -d '{"using":"id","value":"gbqfb"}'
-curl -X POST http://127.0.0.1:4444/wd/hub/session/$1/element/1/click
+curl -X DELETE http://127.0.0.1:4444/wd/hub/session/$session_id/window
